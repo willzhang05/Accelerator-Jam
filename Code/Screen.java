e78eedde787183f6fe094b83bdd4127197c82fdd
Antioch Sanders <antiochsanders@gmail.com>
2015-05-05 00:15:06 -0400

    Screen
    Will fix "extends Jpanel" so multiple Screens can come after each other
    on one Jpanel.
    
    Fade out added but not tested, delay is always zero

---
+++ Code/Screen.java
@@ -0,0 +1,33 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Screen extends Jpanel
+{
+   private long xDim;
+   private long yDim;
+
+   public void paintComponent()
+   {
+      //Create GUI components
+      //Bulk of code goes here
+   }   
+
+   /*Create an opaque black rectangle and 
+   decrease opacity until it is black*/
+   public void fadeOut(Graphics pen, r, g, b, int speed)
+   {
+      for(int opacity = 0; int opacity<255; opacity++)
+         {
+            pen.setColor(new Color(r,g,b,opacity));
+            pen.drawRect(0,0,xDim,yDim);
+	    //delay( 1000 * ( speed / 510 ) );
+
+
+         }
+   }
+
+   public void cleanUp()
+   {
+      //Delete components
+   }
+}
