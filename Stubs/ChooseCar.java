eff13726f947ad988e593b570915bdb75b41e4fc
Antioch Sanders <antiochsanders@gmail.com>
2015-05-21 02:22:44 -0400

    Added BS

--- Stubs/ChooseCar.java
+++ Stubs/ChooseCar.java
@@ -2,3 +2,48 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.image.*;
+
+public class ChooseCar extends JPanel
+{
+
+   public createGUI()
+   {
+   }
+
+   /**
+   * ChooseCar constructor
+   */
+   public ChooseCar()
+   {
+   }
+
+   
+   /**
+   * Selects car right of the currently selected vehicle
+   */
+   public void selectRight()
+   {  
+   }
+   
+   /**
+   * Selects car left of the currently selected vehicle
+   */
+   public void selectLeft()
+   {   
+   }
+
+   /**
+   * Returns selected vehicle 
+   */   
+   public Vehicle select()
+   {
+   }
+   
+   
+
+
+
+
+
+
+}
