313b7696f6aab07f7f033dbdc2971a73aada6833
Antioch Sanders <antiochsanders@gmail.com>
2015-05-21 02:57:17 -0400

    gamepanel BS added

--- Stubs/gamePanel.java
+++ Stubs/gamePanel.java
@@ -11,11 +11,11 @@ public class gamePanel extends main{
 @author William Zhang and Antioch Sanders
 @version 0.0.1
 **/    
-	/*private BufferedImage myImage;
+	 private BufferedImage myImage;
     private Timer updateTimer;
     private int frameRate = 20;
     private final int N = 600;
-    private int position = 0;*/
+    private int position = 0;
     
     public gamePanel(){
 		/**
@@ -24,32 +24,32 @@ public class gamePanel extends main{
 		* Shows after startButton is pressed on the mainMenu.
 		@see mainMenu
 		**/
-        setTitle("Accelerator Jam");
-        setSize(800, 600);
-        setLocationRelativeTo(null);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        /*myImage = new BufferedImage(N, N, BufferedImage.TYPE_INT_RGB);
-        Graphics buffer = myImage.getGraphics();
-        updateTimer = new Timer(1000 / frameRate, new Listener());
-        updateTimer.start();*/
+
     }
-    /*public void paintComponent(Graphics pen){
-        pen.drawImage(myImage, 0, 0, getWidth(), getHeight(), null);
-        drawBackGround(pen);
+    public void paintComponent(Graphics pen){
     }
-    public void drawBackGround(Graphics pen){
-        ImageIcon background = new ImageIcon("perfectRoad.png");
-        pen.drawImage(background.getImage(), 0, 0 + position, 600, 1200 + position, null);
-        
+    public void drawBackGround(Graphics pen){        
     }
     private class Listener implements ActionListener{
         public void actionPerformed(ActionEvent e){
-            update();
-            repaint();
-            System.out.println("Position at: " + position);
+       /**
+       * Get movements
+       **/        
         }
     }
+
+    private class Listener implements ActionListener{
+        public void actionPerformed(ActionEvent e){
+       /**
+       * Check for pause
+       **/        
+        }
+    }
+
+
     public void update(){
-        position++;
-    }*/
+    /**
+    *Update game positions/graphics
+    **/
+    }
 }
