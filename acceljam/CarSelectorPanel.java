0e2acd41a676e8cce61693095fac5b98902e3405
Antioch Sanders <antiochsanders@gmail.com>
2015-06-03 06:53:33 -0400

    Car selector updates
    -Platform
    -Blank methods written in
    -Store info file created

--- acceljam/CarSelectorPanel.java
+++ acceljam/CarSelectorPanel.java
@@ -5,13 +5,16 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
+import java.util.Scanner;
+import java.io.*;
 
 
 public class CarSelectorPanel extends JPanel{
-    public BufferedImage myImage;
-    public Graphics myBuffer;
-    public final int N = 600;
-   
+    private BufferedImage myImage;
+    private Graphics myBuffer;
+    private final int N = 600;
+    public String[] cars;
+
     public CarSelectorPanel(){
         myImage = new BufferedImage(N, N, BufferedImage.TYPE_INT_RGB);
         myBuffer = myImage.getGraphics();
@@ -24,23 +27,19 @@ public class CarSelectorPanel extends JPanel{
     }
 
     public void drawPanel(){
-        
-        int carSpeed = 1000;
-        int carOverallHandling =  34;
-        
         myBuffer.setColor(Color.decode("#EEEEEE"));
         myBuffer.fillRect(0, 0, N, N);
-        myBuffer.setColor(Color.decode("#BBBBBB"));
-        myBuffer.fillOval(100, 50, 400, 400);
-        myBuffer.setColor(new Color(230, 230, 230));
-        myBuffer.fillRect(5, 500, 530, 95);
-        myBuffer.setColor(new Color(0, 230, 80));
-        myBuffer.fillRect(540, 500, 55, 95);
+        ImageIcon myPortrait = new ImageIcon("acceljam\\Resources\\Materials\\circle.png");
+        myBuffer.drawImage(myPortrait.getImage(), 200, 150,
+                                   200 , 266, null);
+        getCarToView();
         myBuffer.setColor(Color.BLACK);
-        myBuffer.drawString("Top Speed: "+carSpeed+"\nHandling: "+carOverallHandling, 15, 515);
-        myBuffer.drawString("Handling: ", 15, 535);        
+        drawStats();
     }
 
-
-
+    private void getCarToView(){}
+    private void drawStats(){}
+    private void getCars(){
+    
+    }
 }
