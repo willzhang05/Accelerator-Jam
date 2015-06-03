3f9bbeb98f660264d12d8e8e7b80e7adf4010e99
Antioch Sanders <antiochsanders@gmail.com>
2015-06-03 19:12:09 -0400

    Patch

--- acceljam/GameOverPanel.java
+++ acceljam/GameOverPanel.java
@@ -11,38 +11,11 @@ import java.util.Random;
 
 public class GameOverPanel extends SmartPanel{
     public final int N = 600;
-    private String[] backgroundOptions = {"crash1.png", "crash2.png", "crash3.png",
-                                          "crash4.png", "crash5.png", "crash6.png",
-                                          "crash7.png", "crash8.png"};
-    private String backgroundPath = "Resources\\Crashes\\";
-//    private String text = "GG Well Played";
-    
+
     public GameOverPanel(JFrame frame){
         super(frame);
-
-        chooseBackground();
         setFocusable(true);
-        //USE GEOMETRY MANAGER, NOT SET LOCATION
-
-//      JButton b = new JButton("Try Again");
-//      b.setLocation(100, 100);
-//      add(b);
-    }
-    
-    public void paintComponent(Graphics pen){
-        ImageIcon crash = new ImageIcon(backgroundPath);
-        pen.drawImage(crash.getImage(), 0, 0, getWidth(), getHeight(), null);
-        pen.setColor(new Color(0,0,0,200));
-        pen.fillRect(0, 0, getWidth(), getHeight());
-        Font boldFont = new Font("Calibri", Font.BOLD, 10);
-        pen.setFont(boldFont);
-        pen.setColor(Color.RED);
-//        pen.drawString(text, 0, 500);
     }
 
-    public void chooseBackground(){
-        Random random = new Random();
-        String choice = backgroundOptions[random.nextInt(backgroundOptions.length)];
-        backgroundPath += choice;
-    }
-}
+    public void paintComponent(Graphics pen){}
+ }
