31adf026b34b73314e69379e4dd16fd91b126759
William Zhang <willzhang05@gmail.com>
2015-06-04 01:01:28 -0400

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	acceljam/mainMenu.class
    
    updated mainMenu design

--- acceljam/mainMenu.java
+++ acceljam/mainMenu.java
@@ -30,18 +30,21 @@ public class mainMenu extends SmartPanel {
     }
     public mainMenu(JFrame frame){
 		super(frame);
-		panel = new JPanel();
-		panel.setLayout(new FlowLayout());
-		panel.setFont(font);
-		panel.setAlignmentX(Component.CENTER_ALIGNMENT);
-		add(panel);
-		
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		setBackground(Color.decode("#D81B60"));
 		label1 = new JLabel("Accelerator Jam");
 		label1.setFont(largeFont);
 		label1.setForeground(Color.decode("#1E88E5"));
 		label1.setAlignmentX(Component.CENTER_ALIGNMENT);
-		panel.add(label1);
-     
+		add(label1);
+		
+		panel = new JPanel();
+		panel.setLayout(new FlowLayout());
+		panel.setFont(font);
+		panel.setAlignmentX(Component.CENTER_ALIGNMENT);
+		panel.setBackground(Color.decode("#D81B60"));
+		add(panel);
+		
 		menuButton button1 = new menuButton("Start", font, new Listener1());
 		panel.add(button1);
 		menuButton button2 = new menuButton("Quit", font, new Listener2());
