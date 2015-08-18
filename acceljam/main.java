80592973307731e97e3c9cb2a2a6a2512b37f809
J3698 <antiochsanders@gmail.com>
2015-08-18 17:42:15 -0400

    -Imports, underconstruction option removed from main

--- acceljam/main.java
+++ acceljam/main.java
@@ -1,23 +1,13 @@
-//Name: William Zhang
+//Name: William Zhang and Antioch Sanders
 //Date: 6/3/15
 //Main Class
 package acceljam;
-import java.awt.EventQueue;
-import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+
 
 public class main {
     public static void main(String[] args) throws Exception {
         frame game = new frame();
-
-        boolean underConstruction = false;
-
-        if(underConstruction)
-            game.setContentPane(new hardHatPanel(game));
-        else
-            game.setContentPane(new mainMenu(game));
-
+        game.setContentPane(new mainMenu(game));
         game.setVisible(true);
 	}
 }
